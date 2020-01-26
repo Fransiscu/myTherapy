3f7e1a2cea32e26aed8eba158b82e565c9244c54
Fransiscu <fran.soru@gmail.com>
2020-01-26 20:32:10 +0100

    Preparazione per Recycleview

--- app/src/main/java/com/ium/mytherapy/controller/SupervisorHomeActivity.java
+++ app/src/main/java/com/ium/mytherapy/controller/SupervisorHomeActivity.java
@@ -1,10 +1,6 @@
 package com.ium.mytherapy.controller;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,9 +9,9 @@ import com.ium.mytherapy.R;
 import com.ium.mytherapy.model.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SupervisorHomeActivity extends AppCompatActivity {
@@ -31,51 +27,24 @@ public class SupervisorHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_supervisore);
         linearLayout = findViewById(R.id.home_supervisore_view);
 
-        ArrayList<User> list = new ArrayList<User>();
+        List<User> list = new ArrayList<>();
 
         User user = new User();
         user.setNome("John");
         user.setCognome("Smith");
 
+        User more = new User();
+        more.setNome("Lorenzo");
+        more.setCognome("Piana");
+
         list.add(user);
+        list.add(more);
 
         cardview = new MaterialCardView(this);
         imageView = new CircleImageView(this);
 
-        ViewGroup.LayoutParams layoutparams = new CardView.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-
-        cardview.setLayoutParams(layoutparams);
-
-        cardview.setMinimumHeight(200);
-
-        cardview.setRadius(8);
-
-        cardview.setPadding(25, 25, 25, 25);
-
-        cardview.setCardBackgroundColor(Color.WHITE);
-
-        textview = new TextView(getApplicationContext());
-
-        textview.setLayoutParams(layoutparams);
-
-        textview.setText(String.format("%s %s", user.getNome(), user.getCognome()));
-
-        textview.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25);
-
-        textview.setPadding(30, 40, 20, 0);
-
-        textview.setGravity(Gravity.CENTER);
-
-        cardview.addView(textview);
-
-        linearLayout.addView(cardview);
-
 
 
     }
-
 
 }
