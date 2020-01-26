e2d7131bf61ee7f7244fc3fe29e364f1b6b1375b
Fransiscu <fran.soru@gmail.com>
2020-01-27 00:54:09 +0100

    Lavorazione per Recycleview

--- app/src/main/java/com/ium/mytherapy/controller/SupervisorHomeActivity.java
+++ app/src/main/java/com/ium/mytherapy/controller/SupervisorHomeActivity.java
@@ -1,17 +1,17 @@
 package com.ium.mytherapy.controller;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.card.MaterialCardView;
 import com.ium.mytherapy.R;
+import com.ium.mytherapy.model.CardViewAdapter;
 import com.ium.mytherapy.model.User;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SupervisorHomeActivity extends AppCompatActivity {
@@ -20,6 +20,7 @@ public class SupervisorHomeActivity extends AppCompatActivity {
     TextView textview;
     LinearLayout linearLayout;
     CircleImageView imageView;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +28,24 @@ public class SupervisorHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_supervisore);
         linearLayout = findViewById(R.id.home_supervisore_view);
 
-        List<User> list = new ArrayList<>();
+        recyclerView = findViewById(R.id.recycler_material_card);
+        CardViewAdapter adapter = new CardViewAdapter(this, User.getObjectList());
+        recyclerView.setAdapter(adapter);
 
-        cardview = new MaterialCardView(this);
-        imageView = new CircleImageView(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//        recyclerView.setLayoutManager(layoutManager);
+
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+//        List<User> list = new ArrayList<>();
+//
+//        cardview = new MaterialCardView(this);
+//        imageView = new CircleImageView(this);
 
 
 
