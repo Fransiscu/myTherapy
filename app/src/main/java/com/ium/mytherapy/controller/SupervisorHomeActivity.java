7676a4a4cfe6bc24160c6e32441cd311fa853df3
Fransiscu <fran.soru@gmail.com>
2020-01-26 14:54:41 +0100

    Classi User, Terapia e Medicina + layout statico supervisorHome pronto

--- app/src/main/java/com/ium/mytherapy/controller/SupervisorHomeActivity.java
+++ app/src/main/java/com/ium/mytherapy/controller/SupervisorHomeActivity.java
@@ -2,15 +2,19 @@ package com.ium.mytherapy.controller;
 
 import android.os.Bundle;
 
+import com.google.android.material.card.MaterialCardView;
 import com.ium.mytherapy.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SupervisorHomeActivity extends AppCompatActivity {
 
+    public MaterialCardView myView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_supervisore);
+
     }
 }
---
+++ app/src/main/java/com/ium/mytherapy/model/User.java
@@ -0,0 +1,62 @@
+package com.ium.mytherapy.model;
+
+public class User {
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private String dataNascita;
+//    private Terapia terapia;
+
+    public User() {
+    }
+
+//    public Terapia getTerapia() {
+//        return terapia;
+//    }
+
+//    public void setTerapia(Terapia terapia) {
+//        this.terapia = terapia;
+//    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(String dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
