2f7a1cdb8a5f82c97e7d45d576ef41657787fac7
Fransiscu <fran.soru@gmail.com>
2020-01-30 22:58:25 +0100

    Logica utenti + aggiunta supervisore

--- app/src/main/java/com/ium/mytherapy/controller/AddUserActivity.java
+++ app/src/main/java/com/ium/mytherapy/controller/AddUserActivity.java
@@ -1,18 +1,141 @@
 package com.ium.mytherapy.controller;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.ium.mytherapy.R;
+import com.ium.mytherapy.model.Supervisor;
+import com.ium.mytherapy.model.User;
+import com.ium.mytherapy.model.UserFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddUserActivity extends AppCompatActivity {
 
+    TextInputEditText nameInput, surnameInput, dateInput, emailInput, usernameInput, passwordInput;
+    TextInputLayout passwordInputLayout;
+    MaterialButton addUserButton;
+
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggiunta_utente);
 
+        nameInput = findViewById(R.id.user_name);
+        surnameInput = findViewById(R.id.user_surname);
+        dateInput = findViewById(R.id.user_data);
+        emailInput = findViewById(R.id.user_email);
+        usernameInput = findViewById(R.id.user_username);
+        passwordInput = findViewById(R.id.user_password);
+        addUserButton = findViewById(R.id.conferma_aggiunta_utente_button);
+
+        passwordInputLayout = findViewById(R.id.user_password_toggle);
+
+        addUserButton.setOnClickListener(view -> {
+            boolean valid = true;
+            String name = Objects.requireNonNull(nameInput.getText()).toString();
+            String surname = Objects.requireNonNull(surnameInput.getText()).toString();
+            String email = Objects.requireNonNull(emailInput.getText()).toString();
+            String username = Objects.requireNonNull(usernameInput.getText()).toString();
+            String password = Objects.requireNonNull(passwordInput.getText()).toString();
+            String birthdate = Objects.requireNonNull(dateInput.getText()).toString();
+
+            if (email.isEmpty()) {
+                emailInput.setError("Inserisci una email valida");
+                valid = false;
+            } else {
+                emailInput.setError(null);
+            }
+            if (name.isEmpty()) {
+                nameInput.setError("Inserisci un nome valido");
+                valid = false;
+            } else {
+                nameInput.setError(null);
+            }
+            if (surname.isEmpty()) {
+                surnameInput.setError("Inserisci un cognome valido");
+                valid = false;
+            } else {
+                surnameInput.setError(null);
+            }
+            if (username.isEmpty()) {
+                usernameInput.setError("Inserisci uno username valido");
+                valid = false;
+            } else {
+                usernameInput.setError(null);
+            }
+            if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+                passwordInputLayout.setPasswordVisibilityToggleEnabled(false);
+                passwordInput.setError("Inserisci password valida");
+                valid = false;
+            } else {
+                passwordInput.setError(null);
+            }
+            if (birthdate.isEmpty()) {
+                dateInput.setError("Inserisci una data");
+                valid = false;
+            } else {
+                dateInput.setError(null);
+            }
+
+            if (valid) {
+                addUser();
+            }
+
+        });
 
     }
+
+    private void addUser() {
+        Runnable signUp = () -> {
+            User user = new User();
+            Supervisor test = new Supervisor(); //TODO: aggiorna con vero valore di supervisore
+            test.setSupervisorId(0);
+            int max = 0;
+            File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/myTherapy/users/");
+
+            /* Setto id supervisore a seconda di quante cartelle ho */
+            File[] files = f.listFiles();
+            if (files != null) {
+                for (File inFile : files) {
+                    if (inFile.isDirectory()) {
+                        String temp = inFile.getName();
+                        if (Integer.parseInt(temp) >= max) {
+                            max = Integer.parseInt(temp) + 1;
+                        }
+                    }
+                }
+                user.setUserId(max);
+            } else {
+                user.setUserId(0);
+            }
+
+            user.setNome(Objects.requireNonNull(nameInput.getText()).toString());
+            user.setCognome(Objects.requireNonNull(surnameInput.getText()).toString());
+            user.setDataNascita(Objects.requireNonNull(dateInput.getText()).toString());
+            user.setEmail(Objects.requireNonNull(emailInput.getText()).toString());
+            user.setUsername(Objects.requireNonNull(usernameInput.getText()).toString());
+            user.setPassword(Objects.requireNonNull(passwordInput.getText()).toString());
+            try {
+                UserFactory.getInstance().addUser(user, test);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(getBaseContext(), "Utente registrato", Toast.LENGTH_LONG).show();
+            finish();
+        };
+
+        signUp.run();
+
+    }
+
 }
--- app/src/main/java/com/ium/mytherapy/model/UserFactory.java
+++ app/src/main/java/com/ium/mytherapy/model/UserFactory.java
@@ -1,8 +1,23 @@
 package com.ium.mytherapy.model;
 
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class UserFactory {
+    private File path = Environment.getExternalStorageDirectory();
+    private File usersDir = new File(path.getAbsolutePath() + "/myTherapy/users/");
+    private File supervisorDir = new File(path.getAbsolutePath() + "/myTherapy/supervisors/");
+    private File baseDir = new File(path.getAbsolutePath() + "/myTherapy/");
 
     private static UserFactory dummy;
 
@@ -16,12 +31,94 @@ public class UserFactory {
         return dummy;
     }
 
-    public User verifyUser() {
+    public User getUser(String userId) throws IOException {
+        return getUserFromFile(usersDir + "/" + userId);
+    }
+
+    public User verifyUser(String username, String password) throws IOException {
+        ArrayList<User> users = this.getUsers();
+
+        if (users == null) {
+            return null;
+        }
+
+        for (User user : Objects.requireNonNull(users)) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+
         return null;
     }
 
-    public User getUser(ArrayList<User> userArrayList) {
-        return null;
+    private ArrayList<User> getUsers() throws IOException {
+        File f = new File(usersDir.toString());
+        ArrayList<User> users = new ArrayList<>();
+
+        /* Scorro tutte le cartelle */
+        File[] files = f.listFiles();
+        if (files != null) {
+            for (File inFile : files) {
+                if (inFile.isDirectory()) {
+                    users.add(getUserFromFile(inFile.toString()));
+                }
+            }
+        } else {
+            return null;
+        }
+        return users;
+    }
+
+    private User getUserFromFile(String filePath) throws IOException {
+        User user = new User();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+
+        /* Leggo dal file e assegno ogni valore al supervisore */
+        String line = bufferedReader.readLine();
+        List<String> strings = Arrays.asList(line.split(","));
+        user.setUserId(Integer.parseInt(strings.get(0)));
+        user.setNome(strings.get(1));
+        user.setCognome(strings.get(2));
+        user.setEmail(strings.get(3));
+        user.setUsername(strings.get(4));
+        user.setPassword(strings.get(5));
+        user.setDataNascita(strings.get(6));
+
+        return user;
+    }
+
+    public void addUser(User user, Supervisor supervisor) throws IOException {
+        File newUser = new File(usersDir.toString() + user.getUserId() + "/");
+
+        boolean wasSuccessful = newUser.mkdirs();
+
+        if (!wasSuccessful) {
+            System.out.println("was not successful.");
+            return;
+        }
+
+        FileOutputStream fos = new FileOutputStream(newUser + "/profile.txt");
+        fos.flush();
+
+        /* Aggiungo file profilo utente */
+        try {
+            FileWriter fw = new FileWriter(newUser + "/profile.txt", true);
+            fw.write(user.getUserId() + "," + user.getNome() + "," + user.getCognome() + "," + user.getEmail() + "," +
+                    user.getUsername() + "," + user.getPassword() + "," + user.getDataNascita());
+            fw.close();
+        } catch (IOException ioe) {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
+
+        /* Aggiungo file al profilo del supervisore */
+        try {
+            FileWriter fw = new FileWriter(supervisorDir + "/" + supervisor.getSupervisorId() + "/utenti.txt");
+            fw.write(user.getUserId() + ",");
+            fw.close();
+        } catch (IOException ioe) {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
+
     }
 
 
