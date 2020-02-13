c6c156bd32b0c97e288f577ef04a5d129015664d
Fransiscu <fran.soru@gmail.com>
2020-02-13 19:52:41 +0100

    Sistemata pagina dettagli medicina

---
+++ app/src/main/java/com/ium/mytherapy/controller/EditAddMedicineActivity.java
@@ -0,0 +1,16 @@
+package com.ium.mytherapy.controller;
+
+import android.os.Bundle;
+
+import com.ium.mytherapy.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditAddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_add_medicine);
+    }
+}
