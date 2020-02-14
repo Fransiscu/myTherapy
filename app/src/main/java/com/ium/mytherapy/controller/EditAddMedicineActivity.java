5a3b807b0e18a8b56a15883ee834c2cfc3d3def9
Fransiscu <fran.soru@gmail.com>
2020-02-14 10:34:06 +0100

    Costruzione pagina aggiunta terapia

--- app/src/main/java/com/ium/mytherapy/controller/EditAddMedicineActivity.java
+++ app/src/main/java/com/ium/mytherapy/controller/EditAddMedicineActivity.java
@@ -1,5 +1,6 @@
 package com.ium.mytherapy.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.ium.mytherapy.R;
@@ -11,6 +12,16 @@ public class EditAddMedicineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_add_medicine);
+        setContentView(R.layout.activity_modifica_terapia);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent userManagementHome = new Intent(getApplicationContext(), UserManagementActivity.class);
+        userManagementHome.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(userManagementHome);
+        finish();
+        overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
     }
 }
