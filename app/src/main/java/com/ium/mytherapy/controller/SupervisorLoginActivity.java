5c2161e8ea0badc2134dc799f2e807c075675bc8
Fransiscu <fran.soru@gmail.com>
2020-02-18 11:34:05 +0100

    SupervisorLoginActivity + SupervisorSignupActivity

--- app/src/main/java/com/ium/mytherapy/controller/SupervisorLoginActivity.java
+++ app/src/main/java/com/ium/mytherapy/controller/SupervisorLoginActivity.java
@@ -23,17 +23,19 @@ public class SupervisorLoginActivity extends AppCompatActivity {
         signup = findViewById(R.id.registrati);
         login = findViewById(R.id.supervisore_login_button);
 
+        /* Listener tasto login, non controllo per nessun dato per ora - TODO: aggiungere logica di verifica */
         login.setOnClickListener(view -> {
             Intent supervisorHomeActivity = new Intent(getApplicationContext(), SupervisorHomeActivity.class);
             supervisorHomeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.SHARED_PREFS, MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString(MainActivity.USER_TYPE, "supervisor");
+            editor.putString(MainActivity.USER_TYPE, "supervisor"); // setto le sharedPreferences
             editor.apply();
             finish();
             startActivity(supervisorHomeActivity);
         });
 
+        /* Listener tasto signup */
         signup.setOnClickListener(view -> {
             Intent newActivity = new Intent(getApplicationContext(), SupervisorSignupActivity.class);
             startActivity(newActivity);
@@ -42,6 +44,7 @@ public class SupervisorLoginActivity extends AppCompatActivity {
         });
     }
 
+    /* Override pressione tasto back per cambiare l'animazione */
     @Override
     public void onBackPressed() {
         super.onBackPressed();
